232787e85d7ecd9a5f164579d3029cc49c3c7eaf
Gwenaelle <75257467+gwendvr@users.noreply.github.com>
2021-03-24 14:52:52 +0100

    Update Nombre premier.py

--- Nombre premier.py
+++ Nombre premier.py
@@ -1,22 +1,26 @@
 # Programme nombre premier
-# date : 24/03/2021
-# auteur : Gwenaelle
-# initialisations
-
-diviseur = 2
-premier = True
+# date : 24/03/2020
+# auteur : Gwen
 
 # saisie du nombre à tester
-val = int(input("entrez un nombre entier > 1 : "))
+val = int(input("entrez un nombre entier > 1 : (0 pour arrêter) "))
 
-# boucle sur la recherche d’un diviseur
-while premier and diviseur * diviseur <= val:
-    if val % diviseur == 0:
-        premier = False
-    diviseur += 1
+while val != 0:
 
-# affichage du message correspondant au nombre
-if premier:
-    print(val, "est premier")
-else:
-    print(val, "n'est pas premier")
+    # initialisations
+    diviseur = 2
+    premier = True
+
+    # boucle sur la recherche d’un diviseur
+    while premier and diviseur * diviseur <= val:
+        if val % diviseur == 0:
+            premier = False
+        diviseur += 1
+
+    # affichage du message correspondant au nombre
+    if premier:
+        print(val, "est premier")
+    else:
+        print(val, "n'est pas premier")
+
+    int(input("entrez un nombre entier > 1 : (0 pour arrêter) "))
